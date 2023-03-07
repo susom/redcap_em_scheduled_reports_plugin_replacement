541a7fe9365cdebdc90cf9998f91d625ea1b5eed
ihabz <ihab.zeedia@stanford.edu>
2023-03-07 12:10:36 -0800

    initial template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scheduled Reports Plugin Replacement
+
+This module will replace an old plugin to send scheduled reports from PID 9342
---
+++ ScheduledReports.php
@@ -0,0 +1,13 @@
+<?php
+namespace Stanford\ScheduledReports;
+
+class ScheduledReports extends \ExternalModules\AbstractExternalModule {
+    public function __construct() {
+        parent::__construct();
+        // Other code to run when object is instantiated
+    }
+
+    public function sendScheduledReportsCron( $cronParameters ) {
+
+    }
+}
